5a5b8d49869ca11b010fd05eed13b53c2fe16037
Omar Khaium Chowdhury <omarkhaium.chowdhury@gmail.com>
2022-08-21 20:15:52 +0600

    updated solution

--- lib/solution.dart
+++ lib/solution.dart
@@ -1,21 +1,14 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    final List<int> results = [];
+    final Map<int, int> map = {};
 
     for (int currentIndex = 0; currentIndex < nums.length; currentIndex++) {
-      final int currentValue = nums.elementAt(currentIndex);
-
-      for (int nextIndex = currentIndex + 1; nextIndex < nums.length; nextIndex++) {
-        final int nextValue = nums.elementAt(nextIndex);
-        if ((currentValue + nextValue) == target) {
-          results.addAll([currentIndex, nextIndex]);
-          break;
-        }
-      }
-      if (results.isNotEmpty) {
-        break;
+      if (map.containsKey(target - nums[currentIndex])) {
+        return [map[target - nums[currentIndex]] ?? -1, currentIndex];
+      } else {
+        map[nums[currentIndex]] = currentIndex;
       }
     }
-    return results;
+    return [];
   }
 }
